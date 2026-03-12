0b75a9140315ae6d6ed96bffb0d91b61f962c6a7
Yuri Fernandes <fszyuri@gmail.com>
2026-03-11 22:30:33 -0300

    for and while

---
+++ aula07for.py
@@ -0,0 +1,17 @@
+# Conta ao contrario (0,6,2) conta de 2 em 2
+# for c in range(6,0, -1):
+#     print(c)
+# print('FIM')
+
+
+# conta de 0 ao numero digitado
+# n = int(input('Digite um numero: '))
+# for c in range(0, n+1):
+#     print(c)
+# print('Fim')
+
+s = 0
+for c in range(0, 4):
+    n = int(input('Digite um numero: '))
+    s += n
+print('O somatorio de todos os valores foi {}'.format(s))
---
+++ ex17.py
@@ -0,0 +1,4 @@
+# Crie um programa que mostre na tela todos os numeros pares de 0 a 100
+
+for c in range(0, 50, 2):
+    print(c)
